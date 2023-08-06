2a3eee76a589ebce03d0bbb4c7de4506604fd0f7
dima <dima.xipkov@gmail.com>
2023-08-07 02:31:27 +0300

    08.08.23,1

--- js/main.js
+++ js/main.js
@@ -6,9 +6,17 @@
 // let attr = buttonModal4.getAttribute("data-mask");
 // console.log(attr);
 
-  
-// 00: 25: 48.958 с
 
+// 00: 25: 48.958 с
+var $pagingInfo = $('.paging-info');
+var $teamSlider = $('.team-slider');
+
+$teamSlider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    var prefix = i < 10 ? '0' : '';
+    var prefixAll = slick.slideCount < 10 ? '0' : '';
+    $pagingInfo.html('<span class="current">' + prefix + i + '</span>' + '' + '<span class="total">' + prefixAll + slick.slideCount + '</span>');
+});
 
 
 // buttonModal4.addEventListener('click', () => {
@@ -21,82 +29,82 @@
 //     el.placeholder = "newAttr";
 // };
 
-const trustCasey = new Swiper('.trustCasey', {
-    slidesPerView: 3,
-    loop: true,
-    loopedslides: 3,
-    centeredSlides: true,
-    // spaceBetween: 30,
-    // slidesPerView: 2,
-    // grid: {
-    //     rows: 2,
-    // },
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    // breakpoints: {
+// const trustCasey = new Swiper('.trustCasey', {
+//     slidesPerView: 3,
+//     loop: true,
+//     loopedslides: 3,
+//     centeredSlides: true,
+//     // spaceBetween: 30,
+//     // slidesPerView: 2,
+//     // grid: {
+//     //     rows: 2,
+//     // },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         type: "fraction",
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     // breakpoints: {
 
-    //     // 1200: {
-    //     //     slidesPerView: 4,
-    //     // },
+//     //     // 1200: {
+//     //     //     slidesPerView: 4,
+//     //     // },
 
-    //     // 992: {
-    //     //     slidesPerView: 3,
+//     //     // 992: {
+//     //     //     slidesPerView: 3,
 
-    //     // },
+//     //     // },
 
-    //     769: {
-    //         slidesPerView: 2,
-    //     },
+//     //     769: {
+//     //         slidesPerView: 2,
+//     //     },
 
-    //     578: {
-    //         slidesPerView: 1,
-    //     },
-    // },
-});
-const galleryCasey = new Swiper('.galleryCasey', {
-    slidesPerView: 3,
-    loop: true,
-    loopedslides: 1,
-    // centeredSlides: true,
-    // spaceBetween: 30,
-    // slidesPerView: 2,
-    // grid: {
-    //     rows: 2,
-    // },
-    autoplay: {
-        delay: 5000,
-    },
+//     //     578: {
+//     //         slidesPerView: 1,
+//     //     },
+//     // },
+// });
+// const galleryCasey = new Swiper('.galleryCasey', {
+//     slidesPerView: 3,
+//     loop: true,
+//     loopedslides: 1,
+//     // centeredSlides: true,
+//     // spaceBetween: 30,
+//     // slidesPerView: 2,
+//     // grid: {
+//     //     rows: 2,
+//     // },
+//     autoplay: {
+//         delay: 5000,
+//     },
 
-    navigation: {
-        nextEl: ".swiper-next",
-        prevEl: ".swiper-prev",
-    },
-    // breakpoints: {
+//     navigation: {
+//         nextEl: ".swiper-next",
+//         prevEl: ".swiper-prev",
+//     },
+//     // breakpoints: {
 
-    //     // 1200: {
-    //     //     slidesPerView: 4,
-    //     // },
+//     //     // 1200: {
+//     //     //     slidesPerView: 4,
+//     //     // },
 
-    //     // 992: {
-    //     //     slidesPerView: 3,
+//     //     // 992: {
+//     //     //     slidesPerView: 3,
 
-    //     // },
+//     //     // },
 
-    //     769: {
-    //         slidesPerView: 2,
-    //     },
+//     //     769: {
+//     //         slidesPerView: 2,
+//     //     },
 
-    //     578: {
-    //         slidesPerView: 1,
-    //     },
-    // },
-});
+//     //     578: {
+//     //         slidesPerView: 1,
+//     //     },
+//     // },
+// });
 
 
 $('.header__btn, .menu a').on('click', function () {
@@ -111,7 +119,7 @@ $('.header__btn').on('click', function () {
     $('.header__btn ').toggleClass('close');
     // для крестика и для меню и что бы закрывалось меню после выбора сcылки
 });
-// 
+//
 // ,
 // $(function () {
 //     let header = $('.header');
@@ -181,4 +189,59 @@ $('.header__btn').on('click', function () {
        
        
        */
+// Overlay
+$('.trust__items').slick({
+    slidesToShow: 3,  /* количество слайдов на показ*/
+    slidesToScroll: 1,
+    // centerMode: true,
+});
 
+$(".gallerys--overlay .js-slick").slick({
+    dots: true,
+    prevArrow: '<button type="button" aria-label="Previous" role="button" class="slick-arrow slick-arrow--prev">Prev</button>',
+    nextArrow: '<button type="button" aria-label="Next" role="button" class="slick-arrow slick-arrow--next">Next</button>',
+    // centerMode: true,
+    // slidesToShow: 3,  /* количество слайдов на показ*/
+    // slidesToScroll: 3,
+    centerPadding: "15%",
+    customPaging: function (slider, i) {
+        var current = i + 1;
+        current = current < 10 ? "0" + current : current;
+
+        var total = slider.slideCount;
+        total = total < 10 ? "0" + total : total;
+
+        return (
+            '<button type="button" role="button" tabindex="0" class="slick-dots-button">\
+			<span class="slick-dots-current">' + current + '</span>\
+			<span class="slick-dots-separator"></span>\
+			<span class="slick-dots-total">' + total + '</span>\
+		</button>'
+        );
+    }
+});
+
+// // Carousel
+$(".gallerys--carousel .js-slick").slick({
+    dots: true,
+    slidesToShow: 3,  /* количество слайдов на показ*/
+    slidesToScroll: 1,
+    centerMode: true,
+    prevArrow: '<button type="button" aria-label="Prev" role="button" class="slick-arrow slick-arrow--prev">Prev</button>',
+    nextArrow: '<button type="button" aria-label="Next" role="button" class="slick-arrow slick-arrow--next">NEXT</button>',
+    customPaging: function (slider, i) {
+        var current = i + 1;
+        current = current < 10 ? "0" + current : current;
+
+        var total = slider.slideCount;
+        total = total < 10 ? "0" + total : total;
+
+        return (
+            '<button type="button" role="button" tabindex="0" class="slick-dots-button">\
+			<span class="slick-dots-current">' + current + '</span>\
+			<span class="slick-dots-separator"></span>\
+			<span class="slick-dots-total">' + total + '</span>\
+		</button>'
+        );
+    }
+});
